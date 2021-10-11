d338bede5426a92df5ccd68ab2d9d9f38b9cfe65
bludya <v.staykov@razorlabs.com>
2021-10-11 19:19:21 +0000

    statesync arguments added

--- docker/full-node/init-full-node.sh
+++ docker/full-node/init-full-node.sh
@@ -22,11 +22,11 @@ sed -i "s/persistent_peers = \".*\"/persistent_peers = \"$PERSISTENT_PEERS\"/g" 
 sed -i "s/seeds = \".*\"/seeds = \"$SEEDS\"/g" "${CUDOS_HOME}/config/config.toml"
 
 if [ "$SHOULD_USE_STATE_SYNC" = "true" ]; then
-    STATE_SYNC_JSON=$(curl -s http://34.123.153.6:26657/commit | jq "{height: .result.signed_header.header.height, hash: .result.signed_header.commit.block_id.hash}")
+    STATE_SYNC_JSON=$(curl -s http://${STATE_SYNC_PEER}/commit | jq "{height: .result.signed_header.header.height, hash: .result.signed_header.commit.block_id.hash}")
     STATE_SYNC_HEIGHT=$(echo $STATE_SYNC_JSON | jq ".height")
     STATE_SYNC_HASH=$(echo $STATE_SYNC_JSON | jq ".hash")
     sed -i "s/enable = false/enable = true/g" "${CUDOS_HOME}/config/config.toml"
-    sed -i "s/rpc_servers = \".*\"/rpc_servers = \"35.224.105.243:26657,34.123.153.6:26657\"/g" "${CUDOS_HOME}/config/config.toml"
+    sed -i "s/rpc_servers = \".*\"/rpc_servers = \"${STATE_SYNC_RPC_SERVERS}\"/g" "${CUDOS_HOME}/config/config.toml"
     sed -i "s/trust_height = 0/trust_height = $STATE_SYNC_HEIGHT/g" "${CUDOS_HOME}/config/config.toml"
     sed -i "s/trust_hash = \".*\"/trust_hash = $STATE_SYNC_HASH/g" "${CUDOS_HOME}/config/config.toml"
 fi
