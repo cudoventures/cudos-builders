badd04e2717a61b5b56c74f804c5bea4adc82aef
Kamen Stoykov <k.stoykov@razorlabs.com>
2022-01-28 18:08:59 +0000

    constructor init + laucher script

--- tools-bash/constructor/src/modules/export-gentx.sh
+++ tools-bash/constructor/src/modules/export-gentx.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -i
 
-timestamp=$(date +%s)
+timestampHumanReadable=$(date '+%Y%m%d%S')
+timestamp=$(date '+%s')
 
 rm -rf "$WORKING_DIR/gen-tx"
 
 if [ $IS_VALIDATOR = "true" ]; then
     mkdir -p "$WORKING_DIR/gen-tx";
     cd "$PARAM_SOURCE_DIR/CudosData/cudos-data-full-node-client-mainnet/config/gentx/";
-    files=(*);
-    cp "$PARAM_SOURCE_DIR/CudosData/cudos-data-full-node-client-mainnet/config/gentx/${files[0]}" "$WORKING_DIR/gen-tx/gentx-$MONIKER-$timestamp.json";
+    firstFile=$(ls | head -1);
+    cp "$PARAM_SOURCE_DIR/CudosData/cudos-data-full-node-client-mainnet/config/gentx/$firstFile" "$WORKING_DIR/gen-tx/gentx-$MONIKER-$timestampHumanReadable-$timestamp.json";
 fi
--- tools-bash/launcher/src/helpers/topology-helper.sh
+++ tools-bash/launcher/src/helpers/topology-helper.sh
@@ -9,73 +9,73 @@ topology=$(cat $LAUNCHER_DIR/config/topology.json)
 # computers
 
 function getComputersSize {
-    echo $topology | python3 -c 'import json, sys; obj = json.load(sys.stdin); print(len(obj["computers"]))'
+    echo $topology | python3 -c 'import json, sys; obj = json.load(sys.stdin); print(len(obj['computers']))'
 }
 
 function getComputerIp {
-    echo $topology
+    echo $topology | python3 -c "import json, sys; obj = json.load(sys.stdin); print(obj['computers'][$1]['id'])"
 }
 
 function getComputerPort {
-    echo $topology   
+    echo $topology | python3 -c "import json, sys; obj = json.load(sys.stdin); print(obj['computers'][$1]['port'])"
 }
 
 function getComputerUser {
-    echo $topology
+    echo $topology | python3 -c "import json, sys; obj = json.load(sys.stdin); print(obj['computers'][$1]['user'])"
 }
 
 function getComputerSshKeyPath {
-    echo $topology
+    echo $topology | python3 -c "import json, sys; obj = json.load(sys.stdin); print(obj['computers'][$1]['sshKey'])"
 }
 
 function getComputerPass {
-    echo $topology
+    echo $topology | python3 -c "import json, sys; obj = json.load(sys.stdin); print(obj['computers'][$1]['pass'])"
 }
 
 # validators
 
-function getValidatorsSize {
-    echo $topology
+function getValidatorComputerId {
+    echo $topology | python3 -c "import json, sys; obj = json.load(sys.stdin); print(obj['nodes']['primary-validator']['computerId'])"
 }
 
-function getValidatorComputerId {
-    echo $topology
+function getValidatorId {
+    echo $topology | python3 -c "import json, sys; obj = json.load(sys.stdin); print(obj['nodes']['primary-validator']['validatorId'])"
 }
 
 function getValidatorOrchEthAddress {
-    echo $topology
+    echo $topology | python3 -c "import json, sys; obj = json.load(sys.stdin); print(obj['nodes']['primary-validator']['orchEthAddress'])"
 }
 
 function getValidatorOrchEthPrivKey {
-    echo $topology
+    echo $topology | python3 -c "import json, sys; obj = json.load(sys.stdin); print(obj['nodes']['primary-validator']['ethPrivKey'])"
 }
 
 # seeds
 
 function getSeedsSize {
-    echo $topology
+    echo $topology | python3 -c "import json, sys; obj = json.load(sys.stdin); print(len(obj['nodes']['seeds']))"
 }
 
 function getSeedValidatorId {
-    echo $topology
+    echo $topology | python3 -c "import json, sys; obj = json.load(sys.stdin); print(obj['nodes']['seeds'][$1]['validatorId'])"
 }
 
 function getSeedComputerId {
-    echo $topology
+    echo $topology | python3 -c "import json, sys; obj = json.load(sys.stdin); print(obj['nodes']['seeds'][$1]['computerId'])"
 }
 
 # sentries
 
 function getSentriesSize {
-    echo $topology
+    echo $topology | python3 -c "import json, sys; obj = json.load(sys.stdin); print(len(obj['nodes']['sentries']))"
 }
 
 function getSentryValidatorId {
-    echo $topology
+    echo $topology | python3 -c "import json, sys; obj = json.load(sys.stdin); print(obj['nodes']['sentries'][$1]['validatorId'])"
 }
 
 function getSentryComputerId {
-    echo $topology
+    echo $topology | python3 -c "import json, sys; obj = json.load(sys.stdin); print(obj['nodes']['sentries'][$1]['computerId'])"
 }
 
 RES=$(getComputersSize)
