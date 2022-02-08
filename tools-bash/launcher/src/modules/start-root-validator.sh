4c14c3b284fe6bfe325aaadf1ed4a7267b6879b4
Kamen Stoykov <k.stoykov@razorlabs.com>
2022-02-08 15:15:42 +0000

    launcher script - restore binary builder

--- tools-bash/launcher/src/modules/start-root-validator.sh
+++ tools-bash/launcher/src/modules/start-root-validator.sh
@@ -22,13 +22,13 @@ unset arg
 result=$(ssh -o "StrictHostKeyChecking no" ${validatorComputerUser}@${validatorComputerIp} -p ${validatorComputerPort} "cd $PARAM_SOURCE_DIR/CudosBuilders/docker/root-node && sudo docker stop $validatorStartContainerName 2> /dev/null")
 ssh -o "StrictHostKeyChecking no" ${validatorComputerUser}@${validatorComputerIp} -p ${validatorComputerPort} "cd $PARAM_SOURCE_DIR && sudo rm -rf ./CudosData"
 
-# echo -ne "Preparing root-validator's binary builder...";
-# result=$(ssh -o "StrictHostKeyChecking no" ${validatorComputerUser}@${validatorComputerIp} -p ${validatorComputerPort} "cd $PARAM_SOURCE_DIR/CudosBuilders/docker/binary-builder && sudo docker-compose --env-file ./binary-builder.arg -f ./binary-builder.yml -p cudos-binary-builder build 2> /dev/null")
-# if [ "$?" != 0 ]; then
-#     echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} There was an error $?: ${result}";
-#     exit 1;
-# fi
-# echo -e "${STYLE_GREEN}OK${STYLE_DEFAULT}";
+echo -ne "Preparing root-validator's binary builder...";
+result=$(ssh -o "StrictHostKeyChecking no" ${validatorComputerUser}@${validatorComputerIp} -p ${validatorComputerPort} "cd $PARAM_SOURCE_DIR/CudosBuilders/docker/binary-builder && sudo docker-compose --env-file ./binary-builder.arg -f ./binary-builder.yml -p cudos-binary-builder build 2> /dev/null")
+if [ "$?" != 0 ]; then
+    echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} There was an error $?: ${result}";
+    exit 1;
+fi
+echo -e "${STYLE_GREEN}OK${STYLE_DEFAULT}";
 
 rootNodeEnv=$(cat $(getValidatorEnvPath))
 monitoringEnabled=$(readEnvFromString "$rootNodeEnv" "MONITORING_ENABLED")
