f037037fc93d485d634c0fa9c465a006e94865aa
mlukanova <meglena.lukanova@gmail.com>
2022-02-21 15:55:52 +0200

    Fix export error

--- tools-bash/constructor/src/modules/export.sh
+++ tools-bash/constructor/src/modules/export.sh
@@ -11,6 +11,5 @@ if [ $IS_VALIDATOR = "true" ]; then
     # firstFile=$(ls | head -1);
     # cp -f "$PARAM_SOURCE_DIR/CudosData/cudos-data-full-node-client-mainnet/config/gentx/$firstFile" "$WORKING_DIR/gen-tx/gentx-$MONIKER-$timestampHumanReadable-$timestamp.json";
     echo $EXPORTED_GENESIS > "$WORKING_EXPORT_DIR/genesis-$MONIKER-$timestampHumanReadable-$timestamp.json";
+    chmod -R 755 "$WORKING_EXPORT_DIR"
 fi
-
-chmod -R 755 "$WORKING_EXPORT_DIR"
