8963efab9d9cb8714fe94d9ec6c963ad1e62f351
infra1515 <lyubomir.yonchev@limechain.tech>
2022-03-06 15:54:00 +0200

    deployment script for explorer-v2 - added branch option + validations for CLI args

--- docker/explorer-v2/big-dipper-2-ui-deploy.sh
+++ docker/explorer-v2/big-dipper-2-ui-deploy.sh
@@ -1,8 +1,18 @@
 env=$1 #prod or dev
+branch=$2
+
+if [ -z "$1" ]; then
+    echo ERROR: "No env arg supplied"
+    exit 1
+fi
+if [ -z "$2" ]; then
+    echo ERROR: "No branch supplied"
+    exit 1
+fi
+
 workdir=$PWD
 echo $workdir
-
-git clone https://github.com/CudoVentures/big-dipper-2.0-cosmos.git $workdir/BigDipper2
+git clone -b $branch https://github.com/CudoVentures/big-dipper-2.0-cosmos.git $workdir/BigDipper2
 cp $workdir/.env-big-dipper-2 $workdir/BigDipper2/.env
 
 cd $workdir/BigDipper2
