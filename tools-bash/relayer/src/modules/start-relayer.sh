fc7d874685a71bf4abbf0a6a930dea3f2738c3e6
Valentin Staykov <v.staykov@razorlabs.com>
2022-03-22 15:54:06 +0000

    ibc tool

--- tools-bash/relayer/src/modules/start-relayer.sh
+++ tools-bash/relayer/src/modules/start-relayer.sh
@@ -7,7 +7,8 @@ echo -e "${STYLE_GREEN}OK${STYLE_DEFAULT}";
 
 echo -ne "Preparing the binary builder...";
 cd "$PARAM_SOURCE_DIR/CudosBuilders/docker/hermes-ibc-relayer"
-dockerResult=$(sudo docker-compose -f ./hermes-ibc-relayer-binary-builder.yml -p hermes-ibc-relayer-binary-builder --env-file ./hermes-ibc-relayer.public.arg build 2> /dev/null)
+dockerResult=$(sudo docker-compose -f ./hermes-ibc-relayer-binary-builder.yml -p hermes-ibc-relayer-binary-builder --env-file ./hermes-ibc-relayer.mainnet.arg build 2> /dev/null))
+
 if [ "$?" != 0 ]; then
     echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} There was an error building the container $?: ${dockerResult}";
     exit 1;
@@ -16,11 +17,11 @@ echo -e "${STYLE_GREEN}OK${STYLE_DEFAULT}";
 
 if [ "$PARAM_INIT" = "true" ]; then
     echo -ne "Stopping previous instances...";
-    docker-compose -f ./hermes-ibc-relayer-init.yml -p hermes-ibc-relayer-init --env-file ./hermes-ibc-relayer.public.arg down 2> /dev/null
+    docker-compose -f ./hermes-ibc-relayer-init.yml -p hermes-ibc-relayer-init --env-file ./hermes-ibc-relayer.mainnet.arg down 2> /dev/null
     echo -e "${STYLE_GREEN}OK${STYLE_DEFAULT}";
 
-    echo -ne "Initialize and starting...";
-    dockerResult=$(docker-compose -f ./hermes-ibc-relayer-init.yml -p hermes-ibc-relayer-init --env-file ./hermes-ibc-relayer.public.arg up --build -d 2> /dev/null)
+    echo -ne "Initialize...";
+    dockerResult=$(docker-compose -f ./hermes-ibc-relayer-init.yml -p hermes-ibc-relayer-init --env-file ./hermes-ibc-relayer.mainnet.arg up --build -d 2> /dev/null)
     if [ "$?" != 0 ]; then
         echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} There was an error building the container $?: ${dockerResult}";
         exit 1;
@@ -30,11 +31,11 @@ fi
 
 if [ "$PARAM_START" = "true" ]; then
     echo -ne "Stopping previous instances...";
-    docker-compose -f ./hermes-ibc-relayer-start.yml -p hermes-ibc-relayer-start --env-file ./hermes-ibc-relayer.public.arg down 2> /dev/null
+    docker-compose -f ./hermes-ibc-relayer-start.yml -p hermes-ibc-relayer-start --env-file ./hermes-ibc-relayer.mainnet.arg down 2> /dev/null
     echo -e "${STYLE_GREEN}OK${STYLE_DEFAULT}";
 
     echo -ne "Starting...";
-    dockerResult=$(docker-compose -f ./hermes-ibc-relayer-start.yml -p hermes-ibc-relayer-start --env-file ./hermes-ibc-relayer.public.arg up --build -d 2> /dev/null)
+    dockerResult=$(docker-compose -f ./hermes-ibc-relayer-start.yml -p hermes-ibc-relayer-start --env-file ./hermes-ibc-relayer.mainnet.arg up --build -d 2> /dev/null)
     if [ "$?" != 0 ]; then
         echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} There was an error building the container $?: ${dockerResult}";
         exit 1;
