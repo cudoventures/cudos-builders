fda094891efc9383b885714ece2f7dad38994755
Kamen Stoykov <k.stoykov@razorlabs.com>
2022-04-06 12:34:03 +0000

    updater: validate version

--- tools-bash/updater/src/incs/validate-upgrade-network.sh
+++ tools-bash/updater/src/incs/validate-upgrade-network.sh
@@ -9,10 +9,16 @@ NETWORK_DRESSREHEARSAL="false"
 NETWORK_TESTNET_PRIVATE="false"
 NETWORK_TESTNET_PUBLIC="false"
 
+NODE_DOCKER_BUILDER_PATH=""
+NODE_ENV_PATH=""
+NODE_ARG_PATH=""
+
 if [ "$PARAM_NODE_NAME" = "root-node" ]; then
     
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/root-node/root-node.mainnet.env" ]; then
-        args=$(cat "$PARAM_SOURCE_DIR/CudosBuilders/docker/root-node/root-node.mainnet.env")
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/root-node/root-node.mainnet.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/root-node/root-node.mainnet.env"
+        args=$(cat "$NODE_ENV_PATH")
         tokenContractAddress=$(readEnvFromString "$args" "CUDOS_TOKEN_CONTRACT_ADDRESS")
         if [ "$tokenContractAddress" = "0x28ea52f3ee46CaC5a72f72e8B3A387C0291d586d" ] || [ "$tokenContractAddress" = "0x12d474723cb8c02bcbf46cd335a3bb4c75e9de44" ]; then
             NETWORK_DRESSREHEARSAL="true"
@@ -29,18 +35,24 @@ if [ "$PARAM_NODE_NAME" = "root-node" ]; then
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/root-node/root-node.dressrehearsal.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/root-node/root-node.dressrehearsal.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/root-node/root-node.dressrehearsal.env"
         NETWORK_DRESSREHEARSAL="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/root-node/root-node.testnet.private.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/root-node/root-node.testnet.private.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/root-node/root-node.testnet.private.env"
         NETWORK_TESTNET_PRIVATE="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
-    if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/root-node/root-node.testnet.public.cone01.env" ]; then
+    if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/root-node/root-node.testnet.public.zone01.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/root-node/root-node.testnet.public.zone01.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/root-node/root-node.testnet.public.zone01.env"
         NETWORK_TESTNET_PUBLIC="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
@@ -51,48 +63,64 @@ fi
 if [ "$PARAM_NODE_NAME" = "seed-node" ]; then
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.testnet.private.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.testnet.private.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.testnet.private.env"
         NETWORK_TESTNET_PRIVATE="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.client.testnet.private01.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.client.testnet.private01.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.client.testnet.private01.env"
         NETWORK_TESTNET_PRIVATE="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.testnet.public.zone01.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.testnet.public.zone01.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.testnet.public.zone01.env"
         NETWORK_TESTNET_PUBLIC="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.testnet.public.zone02.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.testnet.public.zone02.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.testnet.public.zone02.env"
         NETWORK_TESTNET_PUBLIC="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.testnet.public.zone03.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.testnet.public.zone03.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.testnet.public.zone03.env"
         NETWORK_TESTNET_PUBLIC="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.client.testnet.public01.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.client.testnet.public01.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.client.testnet.public01.env"
         NETWORK_TESTNET_PUBLIC="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.client.dressrehearsal.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.client.dressrehearsal.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.client.dressrehearsal.env"
         NETWORK_DRESSREHEARSAL="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.client.mainnet.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.client.mainnet.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.client.mainnet.env"
         args=$(cat "$PARAM_SOURCE_DIR/CudosBuilders/docker/seed-node/seed-node.client.mainnet.arg")
         volumeName=$(readEnvFromString "$args" "VOLUME_NAME")
         if [ -f "$PARAM_SOURCE_DIR/CudosData/$volumeName/config/genesis.json" ]; then
@@ -119,48 +147,64 @@ fi
 if [ "$PARAM_NODE_NAME" = "sentry-node" ]; then
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.testnet.private.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.testnet.private.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.testnet.private.env"
         NETWORK_TESTNET_PRIVATE="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.client.testnet.private01.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.client.testnet.private01.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.client.testnet.private01.env"
         NETWORK_TESTNET_PRIVATE="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.testnet.public.zone01.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.testnet.public.zone01.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.testnet.public.zone01.env"
         NETWORK_TESTNET_PUBLIC="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.testnet.public.zone02.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.testnet.public.zone02.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.testnet.public.zone02.env"
         NETWORK_TESTNET_PUBLIC="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.testnet.public.zone03.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.testnet.public.zone03.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.testnet.public.zone03.env"
         NETWORK_TESTNET_PUBLIC="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.client.testnet.public01.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.client.testnet.public01.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.client.testnet.public01.env"
         NETWORK_TESTNET_PUBLIC="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.client.dressrehearsal.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.client.dressrehearsal.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.client.dressrehearsal.env"
         NETWORK_DRESSREHEARSAL="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.client.mainnet.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.client.mainnet.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.client.mainnet.env"
         args=$(cat "$PARAM_SOURCE_DIR/CudosBuilders/docker/sentry-node/sentry-node.client.mainnet.arg")
         volumeName=$(readEnvFromString "$args" "VOLUME_NAME")
         if [ -f "$PARAM_SOURCE_DIR/CudosData/$volumeName/config/genesis.json" ]; then
@@ -187,36 +231,48 @@ fi
 if [ "$PARAM_NODE_NAME" = "full-node" ]; then
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.client.testnet.private01.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.client.testnet.private01.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.client.testnet.private01.env"
         NETWORK_TESTNET_PRIVATE="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.testnet.public.zone02.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.testnet.public.zone02.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.testnet.public.zone02.env"
         NETWORK_TESTNET_PUBLIC="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.testnet.public.zone03.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.testnet.public.zone03.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.testnet.public.zone03.env"
         NETWORK_TESTNET_PUBLIC="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.client.testnet.public01.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.client.testnet.public01.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.client.testnet.public01.env"
         NETWORK_TESTNET_PUBLIC="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.client.dressrehearsal.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.client.dressrehearsal.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.client.dressrehearsal.env"
         NETWORK_DRESSREHEARSAL="true"
         source "$WORKING_SRC_DIR/incs/validate-upgrade-verify-network.sh"
         networkIdentified="true"
     fi
 
     if [ -f "$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.client.mainnet.env" ]; then
+        NODE_ARG_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.client.mainnet.arg"
+        NODE_ENV_PATH="$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.client.mainnet.env"
         args=$(cat "$PARAM_SOURCE_DIR/CudosBuilders/docker/full-node/full-node.client.mainnet.arg")
         volumeName=$(readEnvFromString "$args" "VOLUME_NAME")
         if [ -f "$PARAM_SOURCE_DIR/CudosData/$volumeName/config/genesis.json" ]; then
@@ -244,5 +300,10 @@ if ([ "$NETWORK_MAINNET" = "false" ] && [ "$NETWORK_DRESSREHEARSAL" = "false" ] 
     echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} Could not identify a network"
     exit 1;
 fi
+
+args=$(cat "$NODE_ARG_PATH")
+VOLUME_NAME=$(readEnvFromString "$args" "VOLUME_NAME")
+START_CONTAINER_NAME=$(readEnvFromString "$args" "START_CONTAINER_NAME")
+unset args
 
 echo -e "${STYLE_GREEN}OK${STYLE_DEFAULT}";
--- tools-bash/updater/src/incs/validate-upgrade-version.sh
+++ tools-bash/updater/src/incs/validate-upgrade-version.sh
@@ -2,10 +2,55 @@
 
 echo -ne "Validating version...";
 
-# get version from container if available
+containerVersion=""
+sourceVersion=""
+fromVersion=""
+toVersion="v0.5.0"
 
-# get version from CudosNode tags if available
+if [ "$(docker container inspect -f '{{.State.Status}}' "$START_CONTAINER_NAME" 2> /dev/null)" = "running" ]; then
+    dockerResult=$(docker container exec "$START_CONTAINER_NAME" /bin/bash -c "cudos-noded version");
+    if [ "$?" = "0" ]; then
+        containerVersion=$(echo "$dockerResult" | cut -d '-' -f1)
+    fi;
+fi
 
-# exit if cannot find the version or if CudosNode|Container verios mismatch
+result=$(cd "$PARAM_SOURCE_DIR/CudosNode" && git describe --tags)
+if [ "$?" = "0" ]; then
+    sourceVersion=$(echo "$result" | cut -d '-' -f1)
+fi;
+
+if [ "$containerVersion" != "" ] && [ "$sourceVersion" = "" ]; then
+    sourceVersion="$containerVersion"
+fi
+
+if [ "$containerVersion" = "" ] && [ "$sourceVersion" != "" ]; then
+    containerVersion="$sourceVersion"
+fi
+
+if [ "$containerVersion" = "" ] && [ "$sourceVersion" = "" ]; then
+    if [ "$NETWORK_MAINNET" = "true" ]; then
+        echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} There is currently no mainnet";
+        exit 1;
+    fi
+    if [ "$NETWORK_DRESSREHEARSAL" = "true" ]; then
+        echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} Dress rehearsal is running the latest version";
+        exit 1;
+    fi
+    if [ "$NETWORK_TESTNET_PRIVATE" = "true" ]; then
+        containerVersion="v0.3.0"
+        sourceVersion="v0.3.0"
+    fi
+    if [ "$NETWORK_TESTNET_PUBLIC" = "true" ]; then
+        containerVersion="v0.4.0"
+        sourceVersion="v0.4.0"
+    fi
+fi
+
+if [ "$containerVersion" != "$sourceVersion" ]; then
+    echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} The source code's version ($sourceVersion) is different from current running docker's version ($containerVersion)";
+    exit 1;
+fi
+
+fromVersion="$sourceVersion"
 
 echo -e "${STYLE_GREEN}OK${STYLE_DEFAULT}";
