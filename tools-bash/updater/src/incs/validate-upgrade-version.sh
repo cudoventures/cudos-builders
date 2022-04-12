9eea38c0d1aca1155a9bf7c2adcc75d35bef2bbd
Kamen Stoykov <k.stoykov@razorlabs.com>
2022-04-12 12:01:01 +0000

    updater: update logging

--- tools-bash/updater/src/incs/validate-upgrade-version.sh
+++ tools-bash/updater/src/incs/validate-upgrade-version.sh
@@ -5,7 +5,7 @@ echo -ne "Validating version...";
 containerVersion=""
 sourceVersion=""
 UPDATE_FROM_VERSION=""
-TO_VERSION="v0.5.0"
+TO_VERSION="v0.6.0"
 
 if [ "$(docker container inspect -f '{{.State.Status}}' "$START_CONTAINER_NAME" 2> /dev/null)" = "running" ]; then
     dockerResult=$(docker container exec "$START_CONTAINER_NAME" /bin/bash -c "cudos-noded version" 2>&1);
--- tools-bash/updater/src/modules/genesis-migrate.sh
+++ tools-bash/updater/src/modules/genesis-migrate.sh
@@ -6,7 +6,7 @@ echo -e "${STYLE_BOLD}Migrating the genesis.json:${STYLE_DEFAULT}";
 
 echo -ne "Preparing the binary builder...";
 cd "$PARAM_SOURCE_DIR/CudosBuilders/docker/binary-builder"
-dockerResult=$(docker-compose --env-file ./binary-builder.arg -f ./binary-builder.yml -p cudos-binary-builder build 2> /dev/null)
+dockerResult=$(docker-compose --env-file ./binary-builder.arg -f ./binary-builder.yml -p cudos-binary-builder build 2>&1)
 if [ "$?" != 0 ]; then
     echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} There was an error building the binary builder for export $?: ${dockerResult}";
     exit 1;
@@ -21,9 +21,9 @@ echo -e "${STYLE_GREEN}OK${STYLE_DEFAULT}";
 
 echo -ne "Starting the container for migration...";
 cd "$NODE_BUILDERS_DOCKER_PATH"
-dockerResult=$(docker-compose --env-file $NODE_ARG_PATH -f ./$START_YML -p ${START_CONTAINER_NAME} up --build -d 2> /dev/null);
+dockerResult=$(docker-compose --env-file $NODE_ARG_PATH -f ./$START_YML -p ${START_CONTAINER_NAME} up --build -d 2>&1);
 if [ "$?" != 0 ]; then
-    echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} There was an error building the sleeping node for export $?: ${dockerResult}";
+    echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} There was an error building the sleeping node for migration $?: ${dockerResult}";
     exit 1;
 fi
 echo -e "${STYLE_GREEN}OK${STYLE_DEFAULT}";
