1f052432d2a3ff61179fc85bb523095e3c03ef1e
Kamen Stoykov <k.stoykov@razorlabs.com>
2022-04-12 12:28:53 +0000

    updater: docker stderr to stdout

--- tools-bash/updater/src/modules/start-node.sh
+++ tools-bash/updater/src/modules/start-node.sh
@@ -6,7 +6,7 @@ echo -e "${STYLE_BOLD}Starting:${STYLE_DEFAULT}";
 
 echo -ne "Starting the node...";
 cd "$NODE_BUILDERS_DOCKER_PATH"
-dockerResult=$(docker-compose --env-file $NODE_ARG_PATH -f ./$START_YML -p ${START_CONTAINER_NAME} up --build -d 2> /dev/null);
+dockerResult=$(docker-compose --env-file $NODE_ARG_PATH -f ./$START_YML -p ${START_CONTAINER_NAME} up --build -d 2>&1);
 if [ "$?" != 0 ]; then
     echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} There was an error building the sleeping node for export $?: ${dockerResult}";
     exit 1;
--- tools-bash/updater/src/modules/start-orchestrator.sh
+++ tools-bash/updater/src/modules/start-orchestrator.sh
@@ -8,7 +8,7 @@ if [ "$HAS_ORCHESTRATOR" = "true" ]; then
     unset args
 
     cd "$PARAM_SOURCE_DIR/CudosBuilders/docker/orchestrator"
-    dockerResult=$(docker-compose --env-file $ORCHESTRATOR_ARG_PATH -f ./orchestrator.release.yml -p $orchestratorContainerName up --build -d 2> /dev/null);
+    dockerResult=$(docker-compose --env-file $ORCHESTRATOR_ARG_PATH -f ./orchestrator.release.yml -p $orchestratorContainerName up --build -d 2>&1);
     if [ "$?" != 0 ]; then
         echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} There was an error starting the orchestrator $?: ${dockerResult}";
         exit 1;
