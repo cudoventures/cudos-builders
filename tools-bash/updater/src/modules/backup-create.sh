42fb11450e7e4d7254d98003c0bda33d7951c7ae
Kamen Stoykov <k.stoykov@razorlabs.com>
2022-04-12 14:52:04 +0000

    updater: backup CudosData

--- tools-bash/updater/src/modules/backup-create.sh
+++ tools-bash/updater/src/modules/backup-create.sh
@@ -5,14 +5,18 @@ echo "" > "$LOCK_BACKUP_CREATE"
 echo -e "${STYLE_GREEN}OK${STYLE_DEFAULT}";
 
 echo -ne "Removing old backups...";
-# rm -rf "$PARAM_SOURCE_DIR/CudosData-backup"
+rm -rf "$PARAM_SOURCE_DIR/CudosData-backup"
 rm -rf "$PARAM_SOURCE_DIR/CudosNode-backup"
 rm -rf "$PARAM_SOURCE_DIR/CudosGravityBridge-backup"
 rm -rf "$PARAM_SOURCE_DIR/CudosBuilders-backup"
 echo -e "${STYLE_GREEN}OK${STYLE_DEFAULT}";
 
 echo -ne "Creating a backup...";
-# \cp -r "$PARAM_SOURCE_DIR/CudosData" "$PARAM_SOURCE_DIR/CudosData-backup"
+\cp -r "$PARAM_SOURCE_DIR/CudosData" "$PARAM_SOURCE_DIR/CudosData-backup"
+if [ "$?" != 0 ]; then
+    echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} Unable to copy $PARAM_SOURCE_DIR/CudosData. Make sure you have enough space.";
+    exit 1;
+fi
 \cp -r "$PARAM_SOURCE_DIR/CudosNode" "$PARAM_SOURCE_DIR/CudosNode-backup"
 \cp -r "$PARAM_SOURCE_DIR/CudosGravityBridge" "$PARAM_SOURCE_DIR/CudosGravityBridge-backup"
 \cp -r "$PARAM_SOURCE_DIR/CudosBuilders" "$PARAM_SOURCE_DIR/CudosBuilders-backup"
