14657ebdae95e558f0605142b8c36b4a472137a6
Kamen Stoykov <k.stoykov@razorlabs.com>
2022-04-14 06:10:06 +0000

    updater: check git error codes

--- tools-bash/updater/src/modules/update-repos.sh
+++ tools-bash/updater/src/modules/update-repos.sh
@@ -15,6 +15,18 @@ echo -ne "Cloning repos...";
 branch="cudos-dev"
 cd "$PARAM_SOURCE_DIR"
 git clone -q --branch "$branch" https://github.com/CudoVentures/cudos-node.git CudosNode &> /dev/null
+if [ "$?" != 0 ]; then
+    echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} There was an error cloning the repo cudos-node. Please try in a while";
+    exit 1;
+fi
 git clone -q --branch "$branch" https://github.com/CudoVentures/cudos-builders.git CudosBuilders &> /dev/null
+if [ "$?" != 0 ]; then
+    echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} There was an error cloning the repo cudos-node. Please try in a while";
+    exit 1;
+fi
 git clone -q --branch "$branch" https://github.com/CudoVentures/cosmos-gravity-bridge.git CudosGravityBridge &> /dev/null
+if [ "$?" != 0 ]; then
+    echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} There was an error cloning the repo cudos-node. Please try in a while";
+    exit 1;
+fi
 echo -e "${STYLE_GREEN}OK${STYLE_DEFAULT}";
