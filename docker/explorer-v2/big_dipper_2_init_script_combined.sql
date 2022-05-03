77aaddc84cc2d83ac6096f927baff3fc7cad35ad
avalkov <avalkov@mail.bg>
2022-05-03 13:58:55 +0300

    Add bdjuno gravity module tables

--- docker/explorer-v2/big_dipper_2_init_script_combined.sql
+++ docker/explorer-v2/big_dipper_2_init_script_combined.sql
@@ -844,3 +844,50 @@ CREATE TABLE proposal_vote_weighted
 CREATE INDEX proposal_vote_weighted_proposal_id_index ON proposal_vote_weighted (proposal_id);
 CREATE INDEX proposal_vote_weighted_voter_address_index ON proposal_vote_weighted (voter_address);
 CREATE INDEX proposal_vote_weighted_height_index ON proposal_vote_weighted (height);
+
+CREATE TABLE delegation
+(
+    validator_address TEXT               NOT NULL REFERENCES validator_info (operator_address),
+    delegator_address TEXT               NOT NULL REFERENCES account (address),
+    amount            COIN               NOT NULL,
+    PRIMARY KEY (validator_address, delegator_address)
+);
+
+CREATE TABLE gravity_orchestrator
+(
+    address TEXT NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE gravity_transaction
+(
+    type TEXT NOT NULL,
+    attestation_id TEXT NOT NULL UNIQUE,
+    orchestrator TEXT NOT NULL REFERENCES gravity_orchestrator (address),
+    receiver TEXT NOT NULL,
+    votes INTEGER NOT NULL,
+    consensus BOOLEAN NOT NULL,
+    transaction_hash TEXT NOT NULL REFERENCES transaction (hash),
+    height BIGINT NOT NULL REFERENCES block (height),
+    PRIMARY KEY(attestation_id, orchestrator)
+);
+
+CREATE INDEX gravity_transaction_receiver_index ON gravity_transaction (receiver);
+CREATE INDEX gravity_transaction_hash_index ON gravity_transaction (transaction_hash);
+CREATE INDEX gravity_transaction_height_index ON gravity_transaction (height);
+
+/*
+ * This function is used to find all gravity transactions associated with given receiver
+ */
+CREATE FUNCTION gravity_messages_by_address(
+    receiver TEXT,
+    "limit" BIGINT = 100,
+    "offset" BIGINT = 0)
+    RETURNS SETOF message AS
+$$
+SELECT m.transaction_hash, m.index, m.type, m.value, m.involved_accounts_addresses
+FROM message m
+         JOIN gravity_transaction t on m.transaction_hash = t.transaction_hash
+WHERE t.receiver = receiver AND t.orchestrator = ANY(m.involved_accounts_addresses)
+ORDER BY t.height DESC
+LIMIT "limit" OFFSET "offset"
+$$ LANGUAGE sql STABLE;
