232cb265efee6143ae10da7e56a2e03aa2fc2e52
Kamen Stoykov <k.stoykov@razorlabs.com>
2022-05-20 09:19:50 +0000

    add scripts for testnet-private v0.8.0

--- tools-bash/upgrade/src/versions/genesis-0.6-0.8.sh
+++ tools-bash/upgrade/src/versions/genesis-0.6-0.8.sh
@@ -77,7 +77,7 @@ if [ "$NETWORK_TESTNET_PRIVATE" = "true" ]; then
     echo $result > "$WORKING_MIGRATE_DIR/genesis.tmp.json"
 
     distributionAddress=$(getModuleAddress "$WORKING_MIGRATE_DIR/genesis.tmp.json" "distribution")
-    setAccountBalanceInAcudosWithoutAuthAccount "$WORKING_MIGRATE_DIR/genesis.tmp.json" "$distributionAddress" "255659558937535318690895660"
+    setAccountBalanceInAcudosWithoutAuthAccount "$WORKING_MIGRATE_DIR/genesis.tmp.json" "$distributionAddress" "257708621488175119214878138"
 
     result=$(jq ".app_state.gov.voting_params.voting_period = \"21600s\"" "$WORKING_MIGRATE_DIR/genesis.tmp.json")
     echo $result > "$WORKING_MIGRATE_DIR/genesis.tmp.json"
