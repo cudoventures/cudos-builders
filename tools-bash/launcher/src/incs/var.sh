6614c7a8a5f4de65067a85bd308d034cf9cf425d
Kamen Stoykov <k.stoykov@razorlabs.com>
2022-05-30 12:03:50 +0000

    merge from cudos-dev

--- tools-bash/launcher/src/incs/var.sh
+++ tools-bash/launcher/src/incs/var.sh
@@ -15,7 +15,7 @@ STYLE_RED='\033[1;31m'
 STYLE_GREEN='\033[1;32m'
 STYLE_DEFAULT='\033[0m'
 
-REPO_BRANCH="cudos-dev"
+REPO_BRANCH="v0.9.0"
 
 if [ ! -f "$WORKING_DIR/config/.env" ]; then
     echo -e "${STYLE_RED}Error:${STYLE_DEFAULT} The $WORKING_DIR/config/.env file is missing";
