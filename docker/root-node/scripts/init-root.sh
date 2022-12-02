b6d1497ceb65f8f370820b57d550a4d8cc20a88b
VStaykov <v.staykov@razorlabs.com>
2022-12-02 08:12:25 +0000

    init root doesn't need admins in genesis params

--- docker/root-node/scripts/init-root.sh
+++ docker/root-node/scripts/init-root.sh
@@ -192,13 +192,6 @@ chmod 600 "${CUDOS_HOME}/zero-account.wallet"
 ZERO_ACCOUNT_ADDRESS=$(echo $KEYRING_OS_PASS | cudos-noded keys show zero-account -a --keyring-backend os)
 cudos-noded add-genesis-account $ZERO_ACCOUNT_ADDRESS "1${BOND_DENOM}"
 
-# create marketplace admin account
-(echo $KEYRING_OS_PASS; echo $KEYRING_OS_PASS) | cudos-noded keys add marketplace-admin-account --keyring-backend os |& tee "${CUDOS_HOME}/marketplace-admin-account.wallet"
-chmod 600 "${CUDOS_HOME}/marketplace-admin-account.wallet"
-MARKETPLACE_ADMIN_ACCOUNT_ADDRESS=$(echo $KEYRING_OS_PASS | cudos-noded keys show marketplace-admin-account -a --keyring-backend os)
-cudos-noded add-genesis-account $MARKETPLACE_ADMIN_ACCOUNT_ADDRESS "1${BOND_DENOM}"
-cat "${CUDOS_HOME}/config/genesis.json" | jq --arg marketplaceAdminAddress "$MARKETPLACE_ADMIN_ACCOUNT_ADDRESS" '.app_state.marketplace.params.admins += [$marketplaceAdminAddress]' > "${CUDOS_HOME}/config/tmp_genesis.json" && mv "${CUDOS_HOME}/config/tmp_genesis.json" "${CUDOS_HOME}/config/genesis.json"
-
 for i in $(seq 1 $NUMBER_OF_VALIDATORS); do
     if [ "$i" = "1" ] && [ "$ROOT_VALIDATOR_MNEMONIC" != "" ]; then
         (echo $ROOT_VALIDATOR_MNEMONIC; echo $KEYRING_OS_PASS) | cudos-noded keys add "validator-$i" --recover --keyring-backend os
