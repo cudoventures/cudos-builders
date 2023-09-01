d8d72e1dbb3177e67e7d1ee5342296d0d93d7ad6
Kamen Stoykov <k.stoykov@razorlabs.com>
2023-09-01 13:58:05 +0000

    update branch name

--- docker/explorer-v2/bdjuno_v4_local_deploy/run.sh
+++ docker/explorer-v2/bdjuno_v4_local_deploy/run.sh
@@ -1,1 +1,1 @@
-./bdjuno-deploy.sh dev http://host.docker.internal:9090 myadminsecret cudos-dev-cosmos-v0.47.3_2
+./bdjuno-deploy.sh dev http://host.docker.internal:9090 myadminsecret cudos-dev-cosmos-v0.47.3
